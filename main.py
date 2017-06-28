76c679be69a206188dd9ef0fe87285d02b9d0aa5
adamFoldvari <foldadam93@gmail.com>
2017-06-28 11:24:14 +0200

    Create buttons and table headers with bootstrap

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for, session, escape
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    table_heads = ['Name',
+                   'Diameter',
+                   'Climate',
+                   'Terrain',
+                   'Surface water percentage',
+                   'Population',
+                   ]
+    response = requests.get('http://swapi.co/api/planets/?page=2').json()
+    print(response['results'][0]['name'])
+    return render_template('index.html', table_heads=table_heads)
 
 
 def main():
