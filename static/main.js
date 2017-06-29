1d184b9e60323779d5a38810dac7269e378797a5
adamFoldvari <foldadam93@gmail.com>
2017-06-29 10:37:18 +0200

    Add function wich calls ajax request for every residents of a planet

--- static/main.js
+++ static/main.js
@@ -7,10 +7,18 @@ $('.btn-primary').on('click', function(){
 
 
 $('#residentsModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
+  var button = $(event.relatedTarget); // Button that triggered the modal
 //   var recipient = button.data('whatever') // Extract info from data-* attributes
-  var planetName = button.data('planet');
-  console.log(planetName)
+  var residentsString = button.data('residents');
+  var residentsSubString = residentsString.substr(1).slice(0, -1).replace(/'/g, "").replace(/ /g, "")
+  var residentsForPlanet = residentsSubString.split(',')
+  Object.values(residentsForPlanet).forEach(function(value) {
+        getResidentAttributes(value);
+    });
+  // for (var i = 0; i < residentsForPlanet.length; i++){
+    
+  // }
+});
 // thats enough for today, take a rest, watch a movie and tommorrow make here ajax requests to get all residents attributes for the planet
 // you should use nested for loops i quess.... good luck
 
@@ -21,4 +29,10 @@ $('#residentsModal').on('show.bs.modal', function (event) {
   // var modal = $(this)
   // modal.find('.modal-title').text('New message to ' + recipient)
   // modal.find('.modal-body input').val(recipient)
-})
+
+
+function getResidentAttributes(url){
+  $.getJSON(url, function(response){
+      console.log(response['name'])
+      });
+}
