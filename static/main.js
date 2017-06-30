f32fc718aa9f7fbe7dc89cb9af5dd79642959a56
adamFoldvari <foldadam93@gmail.com>
2017-06-30 11:51:18 +0200

    Https converter add

--- static/main.js
+++ static/main.js
@@ -15,13 +15,14 @@ $('#residentsModal').on('show.bs.modal', function (event) {
   var residentsSubString = residentsString.substr(1).slice(0, -1).replace(/'/g, "").replace(/ /g, "")
   var residentsForPlanet = residentsSubString.split(',')
   Object.values(residentsForPlanet).forEach(function(value) {
+        value = [value.slice(0, 4), 's', value.slice(4)].join('');
         $('.modal-table-body').empty()
         getResidentAttributes(value);
     });
 });
 
 function getResidentAttributes(url){
-  $.getJSON(url + '/', function(response){
+  $.getJSON(url, function(response){
     var newRowContent = "<tr><td>" + response['name'] + "</td><td>" + response['height'] + "</td><td>" + response['mass'] + "</td><td>" + response['hair_color'] +
     "</td><td>" + response['skin_color'] + "</td><td>" + response['eye_color'] + "</td><td>" + response['birth_year']+ "</td><td>"+ response['gender'] + "</td></tr>"
     $('.modal-table-body').append(newRowContent)
